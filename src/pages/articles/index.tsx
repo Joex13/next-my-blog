f65cfb6e63bc256d11009dd8783a0db02fa2b187
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-14 17:21:36 +0900

    fix: getStaticPathsを静的ページに使用していたミスを修正

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,12 +1,25 @@
+import List from '@/components/pages/List';
 import { client } from '@/libs/client';
-import { List } from '@/components/pages';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
 
-type Props = {};
+type Props = {
+  articles: Article[];
+  infoSearched: InfoSearched;
+};
 
-const PER_PAGE = 10;
+type Article = {
+  id: string;
+  title: string;
+  content: string;
+};
 
-const Index = ({ articles, infoSearched }: any) => {
+type InfoSearched = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+};
+
+const Index = ({ articles, infoSearched }: Props) => {
   return (
     <>
       <List articles={articles} infoSearched={infoSearched} />
@@ -16,25 +29,11 @@ const Index = ({ articles, infoSearched }: any) => {
 
 export default Index;
 
-export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  const articles = await client.get({ endpoint: 'articles' });
-
-  const range = (start: number, end: number) =>
-    [...Array(end - start + 1)].map((_, i) => start + i);
-
-  const paths = range(1, Math.ceil(articles.totalCount / PER_PAGE)).map(
-    (article) => `/articles/list/${article}`
-  );
-
-  return { paths, fallback: false };
-};
-
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  const id = Number(ctx.params?.id);
+export const getStaticProps: GetStaticProps = async () => {
   const articles = await client.get({
     endpoint: 'articles',
     queries: {
-      offset: (id - 1) * 10,
+      offset: 0,
       limit: 10,
     },
   });
