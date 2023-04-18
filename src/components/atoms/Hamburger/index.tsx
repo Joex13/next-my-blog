5bc024a8d0623c1f98a1c5471b505dd03464199a
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-18 16:18:52 +0900

    飯場が

--- src/components/atoms/Hamburger/index.tsx
+++ src/components/atoms/Hamburger/index.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+
 type Props = {};
 
 function Hamburger(props: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = (): void => {
+    setIsOpen((prev) => !prev);
+  };
   return (
-    <button type="button" className="md:hidden z-10 space-y-2">
-      <div className="w-8 h-0.5 bg-red-950" />
-      <div className="w-8 h-0.5 bg-red-950" />
-      <div className="w-8 h-0.5 bg-red-950" />
+    <button
+      type="button"
+      onClick={handleClick}
+      className={
+        isOpen
+          ? 'md:hidden z-10 flex flex-col h-6'
+          : 'md:hidden z-10 flex flex-col justify-between h-6'
+      }
+    >
+      <span
+        className={
+          isOpen
+            ? 'block w-8 h-0.5 bg-slate-950 duration-300 rotate-45'
+            : 'block w-8 h-0.5 bg-slate-950 duration-300'
+        }
+      />
+      <span
+        className={
+          isOpen
+            ? 'block w-8 h-0.5 bg-slate-950 duration-300 opacity-0'
+            : 'block w-8 h-0.5 bg-slate-950 duration-300'
+        }
+      />
+      <span
+        className={
+          isOpen
+            ? 'block w-8 h-0.5 bg-slate-950 duration-300 -rotate-45'
+            : 'block w-8 h-0.5 bg-slate-950 duration-300'
+        }
+      />
     </button>
   );
 }
