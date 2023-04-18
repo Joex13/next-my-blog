3bb703dadec8c6d19003dceb9a03836a31b9c543
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-18 10:51:33 +0900

    css: headerとfooterをダークモードに対応

--- src/components/globals/Footer/index.tsx
+++ src/components/globals/Footer/index.tsx
@@ -1,6 +1,15 @@
+import { useSelector } from 'react-redux';
+
 function Footer() {
+  const isDarkMode = useSelector((state: any) => state.darkMode.isDarkMode);
   return (
-    <footer className="bg-slate-950 p-4 mt-4 w-full">
+    <footer
+      className={
+        isDarkMode
+          ? 'bg-slate-950 w-full p-4 mt-4'
+          : 'bg-orange-400 w-full p-4 mt-4'
+      }
+    >
       <div className="flex justify-between max-w-screen-lg mx-auto">
         <p>this is footer</p>
         <p>this is footer</p>
--- src/components/pages/List/index.tsx
+++ src/components/pages/List/index.tsx
@@ -30,23 +30,25 @@ function List({ articles, infoSearched }: Props) {
   return (
     <div>
       <Header />
-      <main className="pt-12 px-2">
-        <h1 className="font-bold">記事一覧</h1>
-        <Button value={'トグル'} handleClick={toggleDarkMode} />
-        <h2>全{infoSearched.totalCount}件中</h2>
-        <p>{infoSearched.offset}</p>
-        <ul className="flex flex-col gap-4 mt-4">
-          {articles.map((article: Article) => {
-            return (
-              <li key={article.id}>
-                <Link href={`/articles/detail/${article.id}`}>
-                  {article.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        <Pagination totalCount={infoSearched.totalCount} />
+      <main className="pt-20 px-2">
+        <div className='max-w-screen-lg mx-auto'>
+          <h1 className="font-bold">記事一覧</h1>
+          <Button value={'トグル'} handleClick={toggleDarkMode} />
+          <h2>全{infoSearched.totalCount}件中</h2>
+          <p>{infoSearched.offset}</p>
+          <ul className="flex flex-col gap-4 mt-4">
+            {articles.map((article: Article) => {
+              return (
+                <li key={article.id}>
+                  <Link href={`/articles/detail/${article.id}`}>
+                    {article.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <Pagination totalCount={infoSearched.totalCount} />
+        </div>
       </main>
       <Footer />
     </div>
