2119ac95d87f87f68e5a2160fae51332bc300684
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-20 14:11:05 +0900

    refactor: footerにベタ書き

--- src/components/Base/Footer/index.tsx
+++ src/components/Base/Footer/index.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { TextBox } from '@/components/Base';
+import Link from 'next/link';
 
 function Footer() {
   const isDarkMode = useSelector((state: any) => state.darkMode.isDarkMode);
@@ -7,6 +8,16 @@ function Footer() {
     <footer className="p-4 mt-4 bg-orange-200">
       <div className="flex justify-between max-w-screen-lg mx-auto">
         <TextBox>this is footer</TextBox>
+        <nav className="my-auto">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/articles/1">記事一覧</Link>
+            </li>
+            <li>
+              <Link href="/categories">カテゴリ一覧</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   );
